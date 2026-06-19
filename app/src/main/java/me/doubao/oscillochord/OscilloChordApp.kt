3df8624ad64b9aaeba3031d208edfad5539827d4
Zhanyuanium <doubao@outlook.com>
2026-06-20 02:05:54 +0800

    feat: create Koin modules and Application init

--- app/src/main/java/me/doubao/oscillochord/OscilloChordApp.kt
+++ app/src/main/java/me/doubao/oscillochord/OscilloChordApp.kt
@@ -1,5 +1,18 @@
 package me.doubao.oscillochord
 
 import android.app.Application
+import me.doubao.oscillochord.di.dataModule
+import me.doubao.oscillochord.di.domainModule
+import me.doubao.oscillochord.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class OscilloChordApp : Application()
+class OscilloChordApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@OscilloChordApp)
+            modules(domainModule, dataModule, viewModelModule)
+        }
+    }
+}
---
+++ app/src/main/java/me/doubao/oscillochord/di/AppModules.kt
@@ -0,0 +1,30 @@
+package me.doubao.oscillochord.di
+
+import me.doubao.oscillochord.data.SettingsRepository
+import me.doubao.oscillochord.domain.audio.AudioEngine
+import me.doubao.oscillochord.domain.chord.ChordDetector
+import me.doubao.oscillochord.domain.lissajous.LissajousProjector
+import me.doubao.oscillochord.ui.info.InfoViewModel
+import me.doubao.oscillochord.ui.keyboard.KeyboardViewModel
+import me.doubao.oscillochord.ui.oscilloscope.OscilloscopeViewModel
+import me.doubao.oscillochord.ui.settings.SettingsViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val domainModule = module {
+    single { AudioEngine() }
+    factory { ChordDetector() }
+    factory { LissajousProjector() }
+}
+
+val dataModule = module {
+    single { SettingsRepository(androidContext()) }
+}
+
+val viewModelModule = module {
+    viewModel { KeyboardViewModel(get()) }
+    viewModel { OscilloscopeViewModel(get()) }
+    viewModel { InfoViewModel(get()) }
+    viewModel { SettingsViewModel(get()) }
+}
